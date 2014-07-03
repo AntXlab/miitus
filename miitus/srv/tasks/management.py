20dc53db4250de6ad0119af73d696ff092d0f3d6
mission.liao <missionaryliao@gmail.com>
2014-07-03 15:38:47 +0800

    perform db management task before running web server

---
+++ miitus/srv/tasks/management.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from werkzeug.utils import find_modules, import_string
+from cqlengine import Model
+from cqlengine.management import sync_table, create_keyspace
+from cqlengine.models import ModelMetaClass
+from miitus import defs
+from ..core import Core
+
+
+c = Core()
+
+
+@c.worker.task(time_limit=150)
+def prepare_db():
+
+    # init keyspace
+    # TODO: replication-factor:1 is just a dev config
+    create_keyspace(defs.CQL_KEYSPACE_NAME, replication_factor=1)
+
+    # sync table
+    for name in find_modules(defs.MODEL_PACKAGE_ROOT, recursive=True):
+        mod = import_string(name)
+        for item_name in dir(mod):
+            item = getattr(mod, item_name)
+            if type(item) == ModelMetaClass and issubclass(item, Model) and item.__name__ != 'Model':
+                sync_table(item)
+
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 import tornado.ioloop
 from miitus.srv.rest.app import App
+from miitus.srv.tasks import management
+
 
 if __name__ == '__main__':
+    # do management task before running
+    t = management.prepare_db.delay()
+    t.wait()
+
     App().app.listen(9001)
     tornado.ioloop.IOLoop.instance().start()
 
